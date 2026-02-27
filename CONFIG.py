6ed9ce7b38e2d34542ace3aef7bb7fe0ef925143
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-02-27 15:04:59 +0500

    initial commit

---
+++ CONFIG.py
@@ -0,0 +1,13 @@
+POSTGRES_USER='postgres_user'
+POSTGRES_PASSWORD='postgres_password'
+POSTGRES_DB='postgres_db'
+
+BASE_URL="http://localhost:12434"
+MODEL="llama3.2:1B-Q8_0"
+
+GROQ_MODEL='llama-3.3-70b-versatile'
+TEMPERATURE=0.7
+
+GEMINI_MODEL='gemini-2.5-flash-lite'
+
+MAX_TOKENS=150
