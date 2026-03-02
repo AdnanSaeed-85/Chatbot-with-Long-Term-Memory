9c43a99917b6046aef09b81625d4260ee4164f78
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-02 19:32:20 +0500

    sse instead of stdio

--- tools.py
+++ tools.py
@@ -41,5 +41,9 @@ def addition(x, y):
     'add x and y'
     return x + y
 
-if __name__ == '__main__':
-    mcp.run()
+if __name__ == "__main__":
+    mcp.run(
+        transport="sse",
+        host="0.0.0.0",
+        port=8000
+    )
